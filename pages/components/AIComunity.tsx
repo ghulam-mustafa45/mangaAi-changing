63df30f04ec272b89a96052a28a073f8041e5a8d
ghulam-mustafa45 <139062950+ghulam-mustafa45@users.noreply.github.com>
2023-08-28 15:27:22 -0700

    changes

--- pages/components/AIComunity.tsx
+++ pages/components/AIComunity.tsx
@@ -14,7 +14,7 @@ const AIComunity = () => {
      </h1>
             <p  className='  m-auto text-white sm:w-[46%] w-[75%] text-center mt-3 mb-5 sm:text-base text-xs'>Here are beautiful anime images and captivating animated artworks, all created by our skilled community using our AI technology.</p>
             <div className='w-fit  mb-12'>
-                 <img className='w-[75%]  m-auto' src="/images/divu1.svg" alt="error" />
+                 <img className='w-[75%]  m-auto' src="./images/divu1.svg" alt="error" />
             </div>
 
             <button className="rounded-md text-white bg-gradient-to-r from-purple-500 to-green-500 p-2 flex m-auto sm:mt-4 mt-3 pt-3">Create Your Anime Art Now <p className='ml-6 mt-1'><FaArrowRight/></p></button>
